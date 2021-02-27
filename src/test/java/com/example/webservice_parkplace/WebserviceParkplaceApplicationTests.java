b5a52bf5b9a8396f7268cbb7253d89a3b36f17b9
atuerk96 <hamit1@hotmail.de>
2021-02-27 17:21:40 +0100

    Initial commit

---
+++ src/test/java/com/example/webservice_parkplace/WebserviceParkplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservice_parkplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceParkplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
