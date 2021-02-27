3a65b447b4c480114cc9ef71eb2a68379ea2b222
atuerk96 <hamit1@hotmail.de>
2021-02-27 23:17:49 +0100

    maven etc neu geupdated

--- src/main/java/com/example/webservice_parkplace/WebserviceParkplaceApplication.java
+++ src/main/java/com/example/webservice_parkplace/WebserviceParkplaceApplication.java
@@ -2,13 +2,26 @@ package com.example.webservice_parkplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class WebserviceParkplaceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo");
 		//SpringApplication.run(WebserviceParkplaceApplication.class, args);
+		System.out.println("Hi");
+		//SpringApplication.run(WebserviceParkplaceApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloWebService {
+	@GetMapping("/")
+	String hello() {
+		return "Hallo Digga";
 	}
 
 }
